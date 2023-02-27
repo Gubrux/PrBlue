27f9eec47b679a18f205fef9c4496dbb16dc6537
cmdm2022 <cmdm2022.camila.duarte@upa.edu.py>
2023-02-27 13:59:45 -0300

    se agregan rutas en app.py y cambios en materiales.html

--- app.py
+++ app.py
@@ -1,1 +1,36 @@
-print("pollo")
+from flask import Flask, render_template # request
+
+
+# from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)  # db = SQLAlchemy(app)
+
+
+@app.route('/login')
+def login():
+    return render_template('web_login.html')
+
+
+@app.route('/materiales')
+def materiales():
+    return render_template('materiales.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('web_register.html')
+
+
+@app.route('/vistas')
+def vistas_1():
+  return render_template('vistas_1.html')
+
+
+@app.route('/aportes')
+def aportes():
+  return render_template('Mis_aportes.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
